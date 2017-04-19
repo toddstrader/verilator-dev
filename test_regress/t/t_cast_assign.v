9d04a110af7efef70c2225574cfc3e0d3d6c2cd4
Todd Strader <todd.strader@twosigma.com>
2017-04-19 08:45:49 -0400

    Still trying to reproduce

--- test_regress/t/t_cast_assign.v
+++ test_regress/t/t_cast_assign.v
@@ -10,18 +10,31 @@
    assign x.foo = y;
 
 interface foo_intf;
-    logic [7:0] foo;
+   logic [7:0] foo;
+endinterface
+
+typedef struct packed {
+   logic [31:0] bar;
+} some_struct_t;
+
+interface bar_intf;
+   some_struct_t the_struct;
+
+   modport sink
+   (
+      input the_struct
+   );
 endinterface
 
 module t2 (
-   input [31:0] i_data
+   bar_intf.sink i_bar
 );
 
    logic [7:0] foo;
    foo_intf the_foo_intf ();
 
    //assign the_foo_intf.foo = $bits(foo)'(32'd5);
-   `ASSIGN_FOO(the_foo_intf, `CAST(foo, i_data))
+   `ASSIGN_FOO(the_foo_intf, `CAST(foo, i_bar.the_struct.bar))
 
    initial begin
       if (the_foo_intf.foo != 5) $stop;
@@ -33,10 +46,14 @@ endmodule
 
 module t();
 
+   bar_intf the_bar_intf ();
+
+   assign the_bar_intf.the_struct.bar = 32'd5;
+
    t2
    t2
    (
-      .i_data (5)
+      .i_bar (the_bar_intf)
    );
 
 endmodule
