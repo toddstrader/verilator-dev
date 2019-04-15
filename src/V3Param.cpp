c3bee6c13af547593881a96772f215f92f00ad66
Todd Strader <tstrader@hudson-trading.com>
2019-04-15 08:38:28 -0400

    Make hierarchical names work with flattened ASTs

--- src/V3Param.cpp
+++ src/V3Param.cpp
@@ -211,7 +211,7 @@ private:
 	    }
 	}
     }
-    void visitCell(AstCell* nodep, string& hierName);
+    void visitCell(AstCell* nodep, const string& hierName);
     void visitModules() {
 	// Loop on all modules left to process
 	// Hitting a cell adds to the appropriate level of this level-sorted list,
@@ -223,6 +223,8 @@ private:
 	    if (!nodep->user5SetOnce()) {  // Process once; note clone() must clear so we do it again
 		m_modp = nodep;
 		UINFO(4," MOD   "<<nodep<<endl);
+		if (m_modp->hierName().empty())
+		    m_modp->hierName(m_modp->origName());
                 iterateChildren(nodep);
 		// Note above iterate may add to m_todoModps
 		//
@@ -232,9 +234,7 @@ private:
 			AstCell* nodep = *it;
 			if ((nonIf==0 && VN_IS(nodep->modp(), Iface))
 			    || (nonIf==1 && !VN_IS(nodep->modp(), Iface))) {
-			    string hierName = m_modp->hierName().empty() ?
-				m_modp->origName() : m_modp->hierName();
-			    visitCell(nodep, hierName);
+			    visitCell(nodep, m_modp->hierName());
 			}
 		    }
 		}
@@ -533,7 +533,7 @@ public:
 //----------------------------------------------------------------------
 // VISITs
 
-void ParamVisitor::visitCell(AstCell* nodep, string& hierName) {
+void ParamVisitor::visitCell(AstCell* nodep, const string& hierName) {
     // Cell: Check for parameters in the instantiation.
     iterateChildren(nodep);
     if (!nodep->modp()) nodep->v3fatalSrc("Not linked?");
