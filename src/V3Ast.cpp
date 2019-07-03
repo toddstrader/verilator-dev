cdc3b3fb17f31377a7d3c6ddecd55d134a9cf74c
Todd Strader <tstrader@hudson-trading.com>
2019-07-03 11:05:21 -0400

    Remove 'auto'

--- src/V3Ast.cpp
+++ src/V3Ast.cpp
@@ -678,7 +678,7 @@ void AstNode::deleteNode() {
     this->m_nextp = reinterpret_cast<AstNode*>(0x1);
     this->m_backp = reinterpret_cast<AstNode*>(0x1);
     this->m_headtailp = reinterpret_cast<AstNode*>(0x1);
-    for (auto it = m_nums.begin(); it != m_nums.end(); ++it) {
+    for (NumberList::iterator it = m_nums.begin(); it != m_nums.end(); ++it) {
         (*it)->nodep(NULL);
     }
     this->m_op1p = reinterpret_cast<AstNode*>(0x1);
