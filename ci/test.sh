b734f65e2535a00575d9c8f6700d2f5179d411c2
Todd Strader <todd.strader@gmail.com>
2019-10-07 07:58:22 -0400

    More CI scrpits

---
+++ ci/test.sh
@@ -0,0 +1,23 @@
+#"!/bin/bash
+set -e
+
+export DRIVER_FLAGS='-j 0 --quiet --rerun'
+
+case $1 in
+    dist)
+        make examples
+        make test_regress SCENARIOS=--dist
+        ;;
+    *)
+    vlt)
+        make test_regress SCENARIOS=--vlt
+        ;;
+    *)
+    vltmt)
+        make test_regress SCENARIOS=--vltmt
+        ;;
+    *)
+    echo "Usage: test.sh (dist|vlt|vltmt)"
+    exit -1
+    ;;
+esac
